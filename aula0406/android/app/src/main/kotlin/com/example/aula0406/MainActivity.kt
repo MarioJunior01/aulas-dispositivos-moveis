d35cf1f2e77111a0730a51e43c70b7bc6cc3f44a
MarioJunior01 <20233026260@estudantes.ifpr.edu.br>
2025-06-05 18:17:47 -0300

    Estrutura de repetição no dart/flutter

---
+++ aula0406/android/app/src/main/kotlin/com/example/aula0406/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula0406
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ aula0406/lib/provarec.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class ProvaRec extends StatelessWidget {
+  const ProvaRec({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    ThemeData seedColor = ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    );
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 230, 206),
+      appBar: AppBar(
+        title: Center(child: Text("Receita de Bolo de Cenoura")),
+        backgroundColor: const Color.fromARGB(255, 255, 230, 206),
+      ),
+      body: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.30,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height * 0.3,
+                  width: size.width * 0.3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'images/bolo_cenoura.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.star, color: Colors.amber, size: 24),
+                        Icon(Icons.star, color: Colors.amber, size: 24),
+                        Icon(Icons.star, color: Colors.amber, size: 24),
+                        Icon(Icons.star, color: Colors.amber, size: 24),
+                        Icon(Icons.star_half, color: Colors.amber, size: 24),
+                      ],
+                    ),
+                  ),
+                ),
+                Text("4.5"),
+                Text("(250 Avaliações)"),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: size.width * 0.60,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Section(title: "Ingredientes", itens: ["- Cenouras, Ovos, Óleo, Açúcar, Farinha, Fermento","- Cobertura: Açúcar, Chocolate em Pó, Manteiga e Leite.", "NNNAAAAAAAAAAAAAAAAAA"]),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Modo de Preparo:",
+                      style: TextStyle(
+                        color: seedColor.primaryColor,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    " 1. Bata enouras, Ovos, Açúcar e óleo no liquidificador\n 2. Misture os líquidos com açúcar e farinha. Adicione o fermento por último\n3. Asse em forno médio a (180 graus) por 30-40 minutos \n4. Para a cobertura: cozinhe todos os ingredientes em fogo baixo até engrossar. Despeje sobre o bolo quente.",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Section extends StatelessWidget {
+  const Section({super.key, required this.title, required this.itens});
+  final String title;
+  final List<String> itens;
+  @override
+  Widget build(BuildContext context) {
+    ThemeData seedColor = ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: seedColor.primaryColor,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+       for (var item in itens) Text(item),
+      ],
+      
+    );
+  }
+}
